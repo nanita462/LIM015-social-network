7d0b695a7ddeab6a99b6c4ad9dc7d85558609ece
Mariperu <maritzarg2020@gmail.com>
2021-09-21 19:09:04 -0500

    resolviendo conflictos

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,15 +1,14 @@
-<<<<<<< HEAD
-
+/* eslint-disable eol-last */
+/* eslint-disable indent */
 import {
   createUserWEP,
-  //signOut,
-  //getUsersignInWithGoogle,
-  //signInWithFb,
   singInWEP,
-} from "../src/";
+  signInWithGoogle,
+} from '../src/firebase/auth.js';
 
-//Configurando firebase mock
+// Configurando firebase mock
 const firebaseMock = require('firebase-mock');
+
 const mockAuth = new firebaseMock.MockAuthentication();
 mockAuth.autoFlush();
 
@@ -18,8 +17,10 @@ global.firebase = firebaseMock.MockFirebaseSdk(
   () => mockAuth,
 );
 
-describe('Function singInWEP', () => {
-  it('User should log in with e-mail and password', () => createUserWEP('nanita462@gmail.com', '12345678a#')
+describe('Function createUserWEP', () => {
+  it('User should create an account with e-mail and password', () =>
+    // eslint-disable-next-line implicit-arrow-linebreak
+    createUserWEP('nanita462@gmail.com', '12345678a#')
     .then((user) => {
       expect(user.email).toBe('nanita462@gmail.com');
     }));
@@ -30,40 +31,6 @@ describe('Function singInWEP', () => {
     .then((user) => {
       expect(user.email).toBe('nanita462@gmail.com');
     }));
-=======
-/* eslint-disable eol-last */
-/* eslint-disable indent */
-import {
-    createUserWEP,
-    singInWEP,
-    signInWithGoogle,
-} from '../src/firebase/auth.js';
-
-// Configurando firebase mock
-const firebaseMock = require('firebase-mock');
-
-const mockAuth = new firebaseMock.MockAuthentication();
-mockAuth.autoFlush();
-
-global.firebase = firebaseMock.MockFirebaseSdk(
-    () => null,
-    () => mockAuth,
-);
-
-describe('Function createUserWEP', () => {
-    it('User should create an account with e-mail and password', () =>
-        // eslint-disable-next-line implicit-arrow-linebreak
-        createUserWEP('nanita462@gmail.com', '12345678a#')
-        .then((user) => {
-            expect(user.email).toBe('nanita462@gmail.com');
-        }));
-});
-
-describe('Function singInWEP', () => {
-    it('User should log in with e-mail and password', () => singInWEP('nanita462@gmail.com', '12345678a#')
-        .then((user) => {
-            expect(user.email).toBe('nanita462@gmail.com');
-        }));
 });
 
 // describe('Function signInWithGoogle', () => {
@@ -75,10 +42,9 @@ describe('Function singInWEP', () => {
 // });
 
 describe('Function signInWithGoogle', () => {
-    it('User should log in with Google', () => signInWithGoogle()
-        .then((user) => {
-            const providerGoogle = user.providerData[0].providerId;
-            expect(providerGoogle).toBe('google.com');
-        }));
->>>>>>> 12d94095b70f8819c6ce2995970e66eda5d09014
+  it('User should log in with Google', () => signInWithGoogle()
+    .then((user) => {
+      const providerGoogle = user.providerData[0].providerId;
+      expect(providerGoogle).toBe('google.com');
+    }));
 });
