3b4007f67770796ce90972c6e72832d3bc77504e
Mariperu <maritzarg2020@gmail.com>
2021-09-21 19:47:39 -0500

    Corrigiendo errores eslint

--- src/views/hambur.js
+++ src/views/hambur.js
@@ -46,8 +46,4 @@
   <!--HomeIcono - verificar/buscar+ -->
   <script src="https://kit.fontawesome.com/74ba6e58ef.js" crossorigin="anonymous"></script>
 
-<<<<<<< HEAD
-`*/
-=======
 ` */
->>>>>>> 12d94095b70f8819c6ce2995970e66eda5d09014
