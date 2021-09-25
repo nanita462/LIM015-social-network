3b78f3a4367a69c35662027f9ac0815f07e272bd
Mariperu <maritzarg2020@gmail.com>
2021-09-24 19:11:50 -0500

    Avance home

---
+++ src/firebase/firestore.js
@@ -0,0 +1,88 @@
+/* eslint-disable eol-last */
+/* eslint-disable max-len */
+const datePublishPost = () => {
+  const datePost = {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  };
+
+  const timePost = {
+    hour12: 'false', // para formato 24 horas
+    hour: 'numeric',
+    minute: 'numeric',
+  };
+
+  const date = new Date().toLocaleDateString('es-Es', datePost);
+  const time = new Date().toLocaleTimeString('es-Es', timePost);
+  const dateTime = `${date} ${time}`;
+
+  return dateTime;
+};
+
+const orderDate = () => {
+  const year = new Date().getFullYear();
+  const month = `0${new Date().getMonth()}`.slice(-2);
+  const day = `0${new Date().getDate()}`.slice(-2);
+  const hour = `0${new Date().getHours()}`.slice(-2);
+  const minute = `0${new Date().getMinutes()}`.slice(-2);
+  const second = `0${new Date().getSeconds()}`.slice(-2);
+  // eslint-disable-next-line radix
+  return parseInt(`${year}${month}${day}${hour}${minute}${second}`, 0);
+};
+
+export const createNewPost = (photo, name, id, content, counterLikes, postImgUrl) => firebase.firestore().collection('posts').add({
+  photo,
+  name,
+  id,
+  content,
+  counterLikes,
+  date: datePublishPost(),
+  orderDate: orderDate(),
+  postImgUrl,
+});
+
+export const readAllPosts = (cb) => firebase.firestore().collection('posts')
+  .orderBy('orderDate', 'desc')
+  .onSnapshot((querySnapshot) => {
+    const post = querySnapshot.docs.map((doc) => ({
+      idPost: doc.id,
+      ...doc.data(),
+    }));
+    cb(post);
+  });
+
+export const deletePost = (idPost) => firebase.firestore().collection('posts').doc(idPost).delete();
+
+// Subir post y crea coleccion "post"
+export const updatePost = (idpost, valueEdited) => firebase.firestore().collection('posts').doc(idpost).update({
+  content: valueEdited,
+});
+
+export const updatLike = (idpost, counterLikes) => firebase.firestore().collection('posts').doc(idpost).update({
+  counterLikes,
+});
+
+export const createComments = (idpost, photoComment, nameComment, idCommentUser, comment) => firebase.firestore().collection('comments').add({
+  idpost,
+  photoComment,
+  nameComment,
+  idCommentUser,
+  comment,
+  date: datePublishPost(),
+  orderDate: orderDate(),
+});
+
+// Leer comentarios
+export const readAllComments = (cb) => firebase.firestore().collection('comments')
+  .orderBy('orderDate', 'desc')
+  .onSnapshot((querySnapshot) => {
+    const comment = querySnapshot.docs.map((doc) => ({
+      idComment: doc.id,
+      ...doc.data(),
+    }));
+    // console.log(comment);// array de TODOS los coments ingresados
+    cb(comment);
+  });
+
+export const deleteComments = (idcomment) => firebase.firestore().collection('comments').doc(idcomment).delete();
--- src/views/home.js
+++ src/views/home.js
@@ -1,76 +1,285 @@
 /* eslint-disable eol-last */
-export const homeView = () => {
-  const view = `
+import {
+  signOut,
+  userInfo,
+} from '../firebase/auth.js';
+
+import {
+  createNewPost,
+  readAllPosts,
+  // deletePost,
+  // updatePost,
+} from '../firebase/firestore.js';
+
+import {
+  uploadImage,
+} from '../firebase/storage.js';
+
+// Cerrar Sesión
+const logOut = (document) => {
+  const idOut = document.querySelector('#idOut');
+  idOut.addEventListener('click', () => {
+    signOut();
+    alert('Cerrando sesión. :(');
+  });
+};
+
+const createPost = (document) => {
+  const idButtonPublish = document.querySelector('#idButtonPublish');
+  const idPublishBox = document.querySelector('#idPublishBox');
+  const user = userInfo();
+
+  idButtonPublish.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const postContent = document.querySelector('#idPublishBoxText').value;
+    const msgErrorPublish = document.querySelector('.errorPublish');
+
+    if (postContent.charAt(0) === ' ' || postContent === '') {
+      msgErrorPublish.textContent = 'Por favor rellena el campo antes de publicar.';
+    } else {
+      const fileImg = document.querySelector('#fileImg');
+      const image = fileImg.files[0];
+      if (image) {
+        const uploadImg = uploadImage(image, 'photos');
+        uploadImg.on( // Cargando imagen de pc
+          'state_changed',
+          (snapshot) => {
+            const loadingImg = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log(`Upload is ${loadingImg}% done`);
+          },
+          (error) => {
+            console.error(error);
+          },
+          () => {
+            uploadImg.snapshot.ref.getDownloadURL().then((postImgUrl) => {
+              createNewPost(user.photo, user.name, user.id, postContent, [], postImgUrl)
+                .then(() => {
+                  msgErrorPublish.classList.add('hide');
+                  idPublishBox.reset();
+                }).catch((error) => console.log(error));
+            });
+          },
+        );
+      } else {
+        createNewPost(user.photo, user.name, user.id, postContent, [], '')
+          .then(() => {
+            msgErrorPublish.classList.add('hide');
+            idPublishBox.reset();
+          })
+          .catch((error) => console.log(error));
+      }
+    }
+  });
+};
+
+// EDITAR Y BORRAR POST
+// const postFunctions = (post, user) => {
+//   const menuPost = post.querySelector('.show'); // verificar
+//   const secUserSelect = post.querySelector('.secUserSelect'); // verificar
+//   const section = document.createElement('section');
+//   section.classList.add('hide');
+//   menuPost.addEventListener('click', (e) => {
+//     e.preventDefault();
+
+//     // Modal para editar/ elim post
+//     const modal = `<ul class="modalMenu">
+//           <li idpost="${user.idPost}" class="editPost">editar</li>
+//           <strong>|</strong>
+//           <li class="liDeletePost" >borrar</li>
+//           </ul>`;
+//     section.innerHTML = modal;
+//     secUserSelect.appendChild(section);
+//     section.classList.toggle('hide');
+
+//     // eliminar post
+//     const deleteBtn = post.querySelector('.liDeletePost');
+//     deleteBtn.addEventListener('click', () => {
+//       const modalMenu = post.querySelector('.modalMenu');
+//       modalMenu.classList.add('hide');
+//       const newModal = `
+//             <ul class="deleteMenu">
+//             <p>Borrar post?</p>
+//             <section>
+//               <li id="idYes">Si</li>
+//               <strong>|</strong>
+//               <li id="idNo">No</li>
+//             </section>`;
+//       section.innerHTML = '';
+//       section.innerHTML = newModal;
+//       const idYes = post.querySelector('#idYes');
+//       const idNo = post.querySelector('#idNo');
+//       idYes.addEventListener('click', () => deletePost(user.idPost)
+//         .then((resp) => resp)
+//         .catch((err) => console.error(err)));
+//       idNo.addEventListener('click', () => section.classList.add('hide'));
+//     });
+
+//     // editar post
+//     const editPost = post.querySelector('.editPost');
+//     const saveIcon = post.querySelector('.saveIcon');
+//     editPost.addEventListener('click', () => {
+//       const publishedText = post.querySelector('.publishedText');
+//       publishedText.contentEditable = 'true'; // mét. para editar
+//       publishedText.focus();
+//       saveIcon.classList.remove('hide');
+//     });
+//     saveIcon.addEventListener('click', () => {
+//       const publishedText = post.querySelector('.publishedText');
+//       const idPosts = editPost.getAttribute('idpost');
+//       const textPostEdited = publishedText.innerText.trim();
+//       if (textPostEdited !== '') {
+//         publishedText.contentEditable = 'false';
+//         saveIcon.classList.add('hide');
+//         section.classList.toggle('hide');
+//         updatePost(idPosts, textPostEdited);
+//       }
+//     });
+//   });
+// };
+
+// TEMPLATE DE POST PUBLICADO
+const postView = (elem, user) => {
+  // eslint-disable-next-line spaced-comment
+  const view = /*html*/ `
+  <section class="secHeadPost">
+    <section class="secInfoUserPost">
+      <img class="imgUserPost" src=${elem.photo} alt="userImage">
+      <section>
+        <h2 class="userNamePost">${elem.name}</h2>
+        <p>${elem.date}</p>
+      </section>
+    </section>
+
+    <!--VERIFICAR ***-->
+    <section class="secUserSelect" >
+        <button class="buttonMenu ${elem.id === user.id ? 'show' : 'hide'}"></button>
+    </section>
+
+  </section>
+
+  <section class="secMainPost">
+    <section class="secDescriptionPost">
+      <section>
+        <p id="${elem.idPost}" class="publishedText">${elem.content}</p>
+        <span idSaveIcon="${elem.idPost}" class="saveIcon hide"><i class="fas fa-check"></i></span>
+      </section>
+      ${elem.postImgUrl ? `<img  class="postImg" src=${elem.postImgUrl} alt="postImg">` : ''}
+    </section>
+
+    <!-- AQUI COLOCAR SECCION LIKES ***-->
+
+    <form class="createComment hide" id="idFormCreateComment"
+        idCommentPost1="${elem.idPost}" userId="${user.id}" userName="${user.name}" >
+        <img class="image-circle image-circleComment" alt="userimage1" src="${user.photo}">
+        <textarea id="descriptionComment" class="input-comment" placeholder="Leave a comment..."></textarea>
+        <i idCommentPost="${elem.idPost}" class="sendCommentForm far fa-paper-plane"></i>
+    </form>
+
+    <div class="errorComment error"></div>
+    <div class="comments-container hide "></div>
+  </section>
+      `;
+  return view;
+};
+
+// HOME
+export const homeView = (user) => {
+  // eslint-disable-next-line spaced-comment
+  const view = /*html*/ `
   <header class="secHeader" id="idHeader">
-  <section class="secLogoText" id="idNavList">
-  <!--<h1 class="logoText">EcoPunto</h1>-->
-  <a class="logoText" href='#/home'>EcoPunto</a>
-  </section>
-  
-  <section class="navbar" id="idNavList">
-   
-  <a href="#/home"><span data-width="25"class="iconify" data-icon="bx:bxs-home-heart"></span>Inicio</a> 
-  <a href="#/profile"><span data-width="25" class="iconify" data-icon="mdi:account-circle"></span>Mi perfil</a>
-  <a href="#/"><span data-width="25" class="iconify" data-icon="ic:outline-log-in"></span>Cerrar sesión</a> 
-    
-  </section>
-</header>
 
-
-<section class="secHome">
-
-  <section class="secUserInfo">
-
-    <section class="secImgProfile">
-      <img class="imgProfile" src="./img/avatar.png" alt="ImgProfile"
-        style= border-radius: 90px;">
+    <section class="secLogoText" id="idNavList">
+      <a class="logoText" href='#/home'>EcoPunto</a>
     </section>
 
-    <section class="secTextInfo">
-      <h2 class="nameUser">Nombre usuario</h2>
-      <p class="nameUser1">Cel</p>
-      <p class="nameUser1">Productos</p>
-      <p class="nameUser1">Ubicación</p>
+    <section class="navbar" id="idNavList">
+      <a href="#/home">
+        <span data-width="25"class="iconify" data-icon="bx:bxs-home-heart"></span>Inicio</a>
+      <a href="#/profile">
+        <span data-width="25" class="iconify" data-icon="mdi:account-circle"></span>Mi perfil</a>
+      <a id="idOut" href="#/">
+        <span data-width="25" class="iconify" data-icon="ic:outline-log-in"></span>Cerrar sesión</a>
     </section>
-  </section>
+  </header>
 
-  <section class="secHomePublish">
+  <section class="secHome">
 
-    <section class="secPublishBox">
-      <textarea class="boxText" name="" id="idPublishBoxText" cols="30" rows="10"
-        placeholder="¿Qué quieres compartir?"></textarea>
-      
-      <section class="errorPublish"></section>
-      
-      
-      <section class="secBtnBoxText">
-      <figure class="imgFile">
-      <span id= "iconFile" class="iconify" data-icon="noto-v1:framed-picture" data-width="40" data-height="40"></span>
-      <input type="file" id="file_input" />
-      <div class="hoverPhoto"></div>
-      </figure>
-       
-       <!--<button class="inputShare"  id="idButtonDelete" type="button">Borrar</button>-->
-        <button class="inputShare"  id="idButtonPublish" type="button">Publicar</button>
+    <section class="secUserInfo">
+
+      <section class="secImgProfile">
+        <img class="imgProfile" src="${user.photo}" alt="ImgProfile" style= border-radius: 90px;>
+      </section>
+
+      <section class="secTextInfo">
+        <h2 class="nameUser">${user.name}</h2>
+        <p class="nameUser1">999 876 543</p>
+        <p class="nameUser1">Botellas de plástico, papel reciclado.</p>
+        <p class="nameUser1">Lima</p>
       </section>
 
     </section>
 
-    <section class="timelinePosts"></section>
+    <section class="secHomePublish">
 
+      <form class="secPublishBox" id="idPublishBox">
+
+        <textarea class="boxText" name="" id="idPublishBoxText" cols="30" rows="10"
+        placeholder="¿Qué quieres compartir?"></textarea>
+        <!-- Mensaje de error -->
+        <section class="errorPublish"></section>
+
+        <section class="secBtnBoxText" id="idBoxText">
+          <figure class="imgFile">
+            <span id= "iconFile" class="iconify" data-icon="noto-v1:framed-picture"
+            data-width="40" data-height="40"></span>
+            <input type="file" id="fileImg" accept="image/png, image/jpg, image/jpeg"/>
+
+            <!--verificar***-->
+            <section class="hoverPhoto"></section>
+          </figure>
+          <!--<button class="inputShare"  id="idButtonDelete" type="button">Borrar</button>-->
+          <button class="inputShare"  id="idButtonPublish" type="button">Publicar</button>
+        </section>
+
+        <section class="timelinePosts"></section>
+
+      </form>
+
+      <section class="secHomePost"></section>
   </section>
 
-</section>
-
-<!--<footer class="secFooter" id="idSecFooter">
+  <!--<footer class="secFooter" id="idSecFooter">
   ©2021 Developed by <a href="https://github.com/nanita462" target="_blank">Ana Aguilera </a>
   and<a href="https://github.com/Mariperu" target="_blank"> Maritza Rodriguez</a>
-</footer>-->
-    `;
+  </footer>-->
+  `;
 
   const mainHome = document.getElementById('mainContainer');
   mainHome.innerHTML = '';
   mainHome.innerHTML = view;
+
+  createPost(mainHome);
+  logOut(mainHome);
+
+  readAllPosts((posts) => {
+    const secHomePost = mainHome.querySelector('.secHomePost');
+    secHomePost.innerHTML = '';
+    posts.forEach((post) => {
+      const secElem = document.createElement('section');
+      secElem.classList.add('newPost');
+      secElem.innerHTML = postView(post, user);
+
+      // if (post.id === user.id) {
+      //   postFunctions(secElem, post);
+      // }
+      // postLikes(divElem, elem, user); // falta
+      // createPostComments(divElem);
+      // readComments(divElem, user);
+      secHomePost.appendChild(secElem);
+    });
+  });
+
   return mainHome;
 };
 
@@ -99,3 +308,17 @@ export const homeView = () => {
 
       </section>
     </section> */
+
+//   <!-- <section class="secInteractionPost">
+//   <section>
+//     <i class="${elem.counterLikes.includes(user.id) ? 'fas' : 'far'} fa-star"></i>
+//     <p>${elem.counterLikes.length ? elem.counterLikes.length : ''} </p>
+//   </section>
+
+//   <section>
+//     <i class="commentIcon far fa-comments"></i>
+//     <p></p>
+//   </section>
+
+//   <i class="fas fa-share-square"></i>
+// </section>
